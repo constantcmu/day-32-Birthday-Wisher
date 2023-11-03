45b33a45bc01e5331144d5779154f63e0f577754
constantcmu <constant.eng.cmu@gmail.com>
2023-11-03 10:42:15 +0700

    update

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-import smtplib
+# import smtplib
+
+
 #
 # my_email = "aooddy892@gmail.com"
-# password = "gibcvayfuuopvhlv"
-#
-# # my_email = "aooddytest@yahoo.com"
-# # password = "Nt02892664653"
-#
-#
+# password = "vdvsfdllevjzjwme"                 # '8,8j<w56kkAGj:AV1E8QE'     #vdvs fdll evjz jwme
+# #
+# my_email = "aooddytest@yahoo.com"
+# password = "dxzfnsfgdqibagcg"
+# # #
+# #
 # with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
 #     connection.starttls()
 #     connection.login(user=my_email,password=password)
-#     connection.sendmail(from_addr=my_email,
-#                         to_addrs="aooddytest@yahoo.com",
-#                         msg="Subject:Hello\n\nThis is body do my email.")
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="aooddy892@gmail.com",
+#         msg="Subject:Hello\n\nThis is body do my email.")
 
 import datetime as dt
 
@@ -21,10 +24,12 @@ year = now.year
 month = now.month
 day_of_week = now.weekday()
 
-print(day_of_week)
+# print(day_of_week)
 
-date_of_birth = dt.datetime(year=1991, month=6, day=18,hour=6,minute=55)
+# date_of_birth = dt.datetime(year=1991, month=6, day=18,hour=6,minute=55)
 
-print(date_of_birth)
+# print(date_of_birth)
 
+
+print(type(year))
 
